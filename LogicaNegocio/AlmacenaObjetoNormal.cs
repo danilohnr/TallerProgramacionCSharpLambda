4029603e0d7899561f834b17c0e3039753241f7f
Danilo <danilo.noguera@gmail.com>
2022-07-23 09:48:00 -0600

    Inicio de la quinta practica del taller Genericos

---
+++ LogicaNegocio/AlmacenaObjetoNormal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaNegocio
+{
+    public class AlmacenaObjetoNormal
+    {
+        //readonly significa que esta variable solo
+        //se puede inicializar en el constructor
+        private readonly Object[] _datosElemento;
+        private int _contador = 0;
+        //Constructor
+        public AlmacenaObjetoNormal(int tamanno)
+        {
+            //Inicializar el arreglo
+            _datosElemento = new Object[tamanno];
+        }
+    }    
+}
